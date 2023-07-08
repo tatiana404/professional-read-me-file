cf3c8e7405a46951197469736bc01511903298a0
Tetiana Peutina <tatiana.peutina@gmail.com>
2023-07-08 13:12:56 -0300

    Readme finished

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+              {
+              type: 'input',
+              name: 'projectName',
+              message: 'Enter the project name:',
+            },{
+              type: 'input',
+              name: 'description',
+              message: 'Enter a brief description of your project:',
+            },{
+                type: "input",
+                message: "Please give your GitHub profile link.",
+                name: "GitHub"
+            },{
+                type: "list",
+                name: "license",
+                message: "Please select which license you would like to use.",
+                choices : [
+                    "APACHE 2.O",
+                    "BSD 3",
+                    "GVL-GPL 3.0",
+                    "MIT",
+                    "None"
+                ],
+            },{
+              type: 'input',
+              name: 'author',
+              message: 'Enter the author name:',
+            },{
+              type: 'input',
+              name: 'installation',
+              message: 'Enter installation instructions:',
+            },{
+              type: 'input',
+              name: 'usage',
+              message: 'Enter usage instructions:',
+            },{
+              type: 'input',
+              name: 'contributing',
+              message: 'Enter contributing guidelines:',
+            },{
+              type: 'input',
+              name: 'tests',
+              message: 'Enter tests instructions:',
+            },{
+              type: 'input',
+              name: 'questions',
+              message: 'Enter tests instructions:',
+            },
 
-// TODO: Create a function to initialize app
-function init() {}
+])
+ .then((answers) => {
+    // Generate the README content based on user input
+    const readmeContent = generateMarkdown(answers)
 
-// Function call to initialize app
-init();
+  
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md file generated successfully!');
+      }
+    });
+  });
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,119 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "None") {
+    return "";
+  }
+
+  if(license === "APACHE 2.O") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  }
+  if(license === "BSD 3") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  }
+  if(license === "GVL-GPL 3.0") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
+  if(license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "None") {
+    return "";
+
+  }
+  else {
+    return `[License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "None") {
+    return "";
+  }
+  else {
+    return `## License ${license}`
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+# ${answers.projectName}
 
+${answers.description}
+
+${answers.GitHub}
+
+${renderLicenseBadge(answers.license)}
+
+
+
+
+## User Story
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+
+## Acceptance Criteria
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+ ${renderLicenseLink(answers.license)}
+- [Author](#author)
+
+## Author
+
+${answers.author}
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+${answers.questions}
+
+
+${renderLicenseSection(answers.license)}
 `;
 }
 
